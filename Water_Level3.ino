82145bd8bfdb6c8b7c29d980872e053c542c2136
D.N. Zaki Zamani <95016412+dnzakizamani@users.noreply.github.com>
2023-11-20 09:39:07 +0700

    Update Water_Level3.ino

--- Water_Level3.ino
+++ Water_Level3.ino
@@ -6,30 +6,26 @@
 
 #define MAX_TRYING 3
 
-// byte mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
-// byte arduinoIP[] = { 192, 168, 28, 190 };    
-// byte gatewayIP[] = { 192, 168, 29, 254 };
-// byte subnetIP[] = { 255, 255, 254, 0 };
-// byte dnsIP[] = { 8, 8, 8, 8 };
-uint8_t mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xEE };
-uint8_t arduinoIP[] = { 192, 168, 28, 190 };    
-uint8_t gatewayIP[] = { 192, 168, 29, 254 };
-uint8_t subnetIP[] = { 255, 255, 254, 0 };
-uint8_t dnsIP[] = { 8, 8, 8, 8 };
-IPAddress server_addr(192, 168, 1, 13);
+uint8_t mac_addr[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
+uint8_t arduinoIP[] = { 192, 168, x, x };
+uint8_t gatewayIP[] = { 192, 168, x, x };
+uint8_t subnetIP[] = { 255, 255, x, x };
+uint8_t dnsIP[] = { x, x, x, x };
+IPAddress server_addr(192, 168, x, x);
 
-char user[] = "root";           
-char password[] = "Bc8574";
+char user[] = "root";
+char password[] = "";
 
 EthernetClient client;
 MySQL_Connection conn((Client *)&client);
 
-
-
-LiquidCrystal_I2C lcd(0x27, 16, 2); // Kalau gagal ganti 0x3F ke 0x27 for
-SoftwareSerial mySerial(11, 10); // RX, TX
+SoftwareSerial mySerial(6, 5);
 unsigned char data[4] = {};
 float distance;
+
+unsigned long pumpStartTime = 0;
+unsigned long pumpOnTime = 0;
+bool malfHandled = false;
 
 void setup()
 {
@@ -37,14 +33,9 @@ void setup()
   mySerial.begin(9600);
   pinMode(7, OUTPUT);
 
-  lcd.init();                      // initialize the lcd
-  lcd.backlight();
-
-  // Add the following lines to establish the Ethernet connection
   Ethernet.begin(mac_addr, arduinoIP, dnsIP, gatewayIP, subnetIP);
   delay(1000);
 
-  // Establish a connection to the MySQL server
   if (conn.connect(server_addr, 3306, user, password))
   {
     Serial.println("Connected to MySQL server!");
@@ -53,25 +44,17 @@ void setup()
   {
     Serial.println("Connection failed.");
   }
-
- 
-
 }
 
 void loop()
 {
-  
-  ultra();
-}
-
-void ultra() {
-  do {
+  do
+  {
     for (int i = 0; i < 4; i++)
     {
       data[i] = mySerial.read();
     }
   } while (mySerial.read() == 0xff);
-
   mySerial.flush();
 
   if (data[0] == 0xff)
@@ -83,35 +66,39 @@ void ultra() {
       distance = (data[1] << 8) + data[2];
       if (distance > 30)
       {
-
         int jarak = distance / 10;
-
         if (jarak <= 15)
         {
-          lcd.clear();
+          if (pumpStartTime != 0)
+          { 
+            pumpStartTime = 0;
+            recordPumpEvent("OFF");
+          }
           digitalWrite(7, HIGH);
-          lcd.setCursor(0, 0);
-          lcd.print("Air Penuh");
           Serial.println("Air Penuh");
-          lcd.setCursor(0, 1);
-          lcd.print("Terima Kasih");
           delay(2000);
-          lcd.clear();
         }
-        else if (jarak >= 65 && jarak <=80)
+        else if (jarak >= 65 && jarak <= 80)
         {
-          lcd.clear();
+          if (pumpStartTime == 0)
+          {
+            pumpStartTime = millis();
+            recordPumpEvent("ON");
+          }
+
           digitalWrite(7, LOW);
-          lcd.setCursor(0, 1);
-          lcd.print("Pompa Menyala");
           Serial.println("Isi");
         }
-        else if (jarak >= 80)
+        else if (jarak > 80 && !malfHandled)
         {
-          lcd.clear();
-          lcd.setCursor(0, 1);
-          lcd.print("Pompa Malfunc");
           Serial.println("Pompa Malfunc");
+          recordPumpEvent("MALF");
+          malfHandled = true;
+        }
+        
+        if (jarak <= 80)
+        {
+          malfHandled = false;
         }
 
         delay(500);
@@ -128,3 +115,27 @@ void ultra() {
   }
   delay(100);
 }
+
+void recordPumpEvent(const char *status)
+{
+   char query[128];
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+
+  if (strcmp(status, "OFF") == 0)
+  {
+    sprintf(query, "INSERT INTO db_sensor.t_pompa (pos, area, status, waktu) VALUES (1, 'Area1', 'OFF', NOW())");
+  }
+  else if (strcmp(status, "ON") == 0)
+  {
+    sprintf(query, "INSERT INTO db_sensor.t_pompa (pos, area, status, waktu) VALUES (1, 'Area1', 'ON', NOW())");
+  }
+  if (strcmp(status, "MALF") == 0)
+  {
+    sprintf(query, "INSERT INTO db_sensor.t_pompa (pos, area, status, waktu) VALUES (1, 'Area1', 'MALF', NOW())");
+  }
+
+  cur_mem->execute(query);
+  delete cur_mem; 
+}
+
+
